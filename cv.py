93749eae9050d35a9c6b3eb6d6dd0b3ecf5af4fa
m-guggenmos <monaden@gmail.com>
2017-08-03 12:19:00 +0200

    First upload

--- cv.py
+++ cv.py
@@ -1,5 +1,80 @@
 import numpy as np
 
+class ShuffleBin:
+
+    def __init__(self, labels, n_iter=10, n_pseudo=5):
+
+        """
+
+        Parameters
+        ----------
+        labels : List<int>, np.ndarray()
+            Label for each trial
+        n_iter : int
+            Number of permutations
+        n_pseudo : int
+            How many trials belong to one bin (aka pseudo-trial)
+        """
+
+        self.labels = np.array(labels)
+        self.n_iter = n_iter
+
+        self.classes, self.n_trials = np.unique(labels, return_counts=True)
+        self.n_classes = self.classes.shape[0]
+        self.n_pseudo = n_pseudo
+        self._compute_pseudo_info()
+
+    def _compute_pseudo_info(self):
+        """
+        Compute indices and labels for the pseudo-trial matrix
+        The pseudo-trial matrix is the resulting matrix *after* having grouped the data into
+        randomly permuted pseudo-trial bins and averaging trials within each bin. Thus, no
+        additional permutation is necessary for pseudo-trial indices, which are somewhat trivial.
+        """
+        self.ind_pseudo_test = np.full((self.n_classes, self.n_classes, 2 * self.n_pseudo),
+                                       np.nan, dtype=np.int)
+        self.labels_pseudo_test = np.full((self.n_classes, self.n_classes, 2 * self.n_pseudo),
+                                          np.nan, dtype=np.int)
+
+        for c1 in range(self.n_classes):
+            range_c1 = range(c1*self.n_pseudo, (c1+1)*self.n_pseudo)
+            for c2 in range(self.n_classes):
+                range_c2 = range(c2*self.n_pseudo, (c2+1)*self.n_pseudo)
+                self.ind_pseudo_test[c1, c2, :2 * self.n_pseudo] = np.concatenate((range_c1, range_c2))
+                self.labels_pseudo_test[c1, c2, :2 * self.n_pseudo] = \
+                    np.concatenate((self.classes[c1] * np.ones(self.n_pseudo),
+                                    self.classes[c2] * np.ones(self.n_pseudo)))
+        self.ind_pseudo_train = []
+        self.labels_pseudo_train = []
+
+
+    def __iter__(self):
+
+        """
+        Generator function for the cross-validation object. Each fold corresponds to a new random
+        grouping of trials into pseudo-trials.
+        """
+        _ind_test = np.full(self.n_classes*self.n_pseudo, np.nan, dtype=np.object)
+        _ind_train = []
+        for perm in range(self.n_iter):
+            for c1 in range(self.n_classes):  # separate permutation for each class
+                prm = np.array(np.array_split(np.random.permutation(self.n_trials[c1]), self.n_pseudo))
+                ind = prm + np.sum(self.n_trials[:c1])
+                for i, j in enumerate(range(c1*self.n_pseudo, (c1+1)*self.n_pseudo)):
+                    _ind_test[j] = ind[i]
+            yield _ind_train, _ind_test
+
+
+    def split(self, X, y=None):
+        return self.__iter__()
+
+    def __len__(self):
+        return self.n_iter
+
+    def get_n_splits(self):
+        return self.n_iter
+
+
 class ShuffleBinLeaveOneOut:
 
     def __init__(self, labels, n_iter=10, n_pseudo=5):
@@ -75,3 +150,4 @@ class ShuffleBinLeaveOneOut:
 
     def __len__(self):
         return self.n_iter
+
--- dissimilarity.py
+++ dissimilarity.py
@@ -1,6 +1,152 @@
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, _class_cov, _class_means, \
     linalg
+from sklearn.base import BaseEstimator
+from scipy.spatial.distance import euclidean, correlation
+
+
+class Euclidean2(BaseEstimator):
+
+    _estimator_type = 'distance'
+
+    def __init__(self, random_state=None, verbose=0):
+
+        self.random_state = random_state
+        self.verbose = verbose
+
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X, y):
+
+        X = np.array(X)
+        self.classes_ = np.unique(y)
+
+        return euclidean(np.mean(X[y == self.classes_[0]], axis=0),
+                         np.mean(X[y == self.classes_[1]], axis=0))**2
+
+
+class CvEuclidean2(BaseEstimator):
+
+    _estimator_type = 'distance'
+
+    def __init__(self, random_state=None, verbose=0):
+
+        self.random_state = random_state
+        self.verbose = verbose
+
+    def fit(self, X, y):
+
+        X = np.array(X)
+        y = np.array(y)
+
+        self.classes_ = np.unique(y)
+
+        self.dist_train = np.mean(X[y == self.classes_[0]], axis=0) - np.mean(X[y == self.classes_[1]], axis=0)
+
+        return self
+
+    def predict(self, X, y):
+
+        X = np.array(X)
+        y = np.array(y)
+
+        dist_test = np.mean(X[y == self.classes_[0]], axis=0) - np.mean(X[y == self.classes_[1]], axis=0)
+
+        return self.dist_train @ dist_test
+
+
+class Pearson(BaseEstimator):
+
+    _estimator_type = 'distance'
+
+    def __init__(self, random_state=None, verbose=0, return_1d=False):
+
+        self.random_state = random_state
+        self.verbose = verbose
+        self.return_1d = return_1d
+
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X, y):
+
+        X = np.array(X)
+        self.classes_ = np.unique(y)
+
+        d = correlation(np.mean(X[y == self.classes_[0]], axis=0),
+                        np.mean(X[y == self.classes_[1]], axis=0))
+
+        if self.return_1d:
+            d = np.atleast_1d(d)
+
+        return d
+
+
+class CvPearson(BaseEstimator):
+
+    _estimator_type = 'distance'
+
+    def __init__(self, random_state=None, verbose=0, regularize_var=True, regularize_denom=True,
+                 reg_factor_var=0.1, reg_factor_denom=0.25, bounded=True, reg_bounding=1,
+                 return_1d=False):
+
+        self.random_state = random_state
+        self.verbose = verbose
+        self.regularize_var = regularize_var
+        self.regularize_denom = regularize_denom
+        self.reg_factor_var = reg_factor_var
+        self.reg_factor_denom = reg_factor_denom
+        self.bounded = bounded
+        self.reg_bounding = reg_bounding
+        self.return_1d = return_1d
+
+    def fit(self, X, y):
+
+        X = np.array(X)
+        y = np.array(y)
+
+        self.classes_ = np.unique(y)
+
+        self.A1 = np.mean(X[y == self.classes_[0]], axis=0)
+        self.B1 = np.mean(X[y == self.classes_[1]], axis=0)
+        self.var_A1 = np.var(self.A1)
+        self.var_B1 = np.var(self.B1)
+        self.denom_noncv = np.sqrt(self.var_A1 * self.var_B1)
+
+        return self
+
+    def predict(self, X, y=None):
+
+        X = np.array(X)
+
+        A2 = np.mean(X[y == self.classes_[0]], axis=0)
+        B2 = np.mean(X[y == self.classes_[1]], axis=0)
+
+        cov_a1b2 = np.cov(self.A1, B2)[0, 1]
+        cov_b1a2 = np.cov(self.B1, A2)[0, 1]
+        cov_ab = (cov_a1b2 + cov_b1a2) / 2
+
+        var_A12 = np.cov(self.A1, A2)[0, 1]
+        var_B12 = np.cov(self.B1, B2)[0, 1]
+
+        if self.regularize_var:
+            denom = np.sqrt(max(self.reg_factor_var * self.var_A1, var_A12) * max(self.reg_factor_var * self.var_B1, var_B12))
+        else:
+            denom = np.sqrt(var_A12 * var_B12)
+        if self.regularize_denom:
+            denom = max(self.reg_factor_denom * self.denom_noncv, denom)
+
+        r = cov_ab / denom
+
+        if self.bounded:
+            r = min(max(-self.reg_bounding, r), self.reg_bounding)
+
+        d = 1 - r
+        if self.return_1d:
+            d = np.atleast_1d(d)
+        return d
+
 
 
 class LDA(LinearDiscriminantAnalysis):
